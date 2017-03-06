638da036482bbdc661e0703647d31d74a7dda437
gaurav <sharma02gaurav@gmail.com>
2017-03-06 17:25:40 +0530

    integrating socket.io

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-var app = require ('express')(),
+var express = require ('express'),
+    app = express(),
     parser = require ('body-parser'),
-    morgan = require ('morgan'),
     expressSession = require ('express-session'),
     cookieParser = require ('cookie-parser');
 
-var UserRouter = require ('./routes/user-routes');
-var ChatRouter = require ('./routes/chat-routes');
+var server = require ('http').createServer (app);
+var io = require ('socket.io')(server);
 
-app.use (parser.json());
-app.use (cookieParser());
-app.use (expressSession ({secret: 'winteriscoming', resave: false, saveUninitialized: true, cookie: {maxAge: 60000}}));
+ app.use (express.static (__dirname+ '/views'));
+    app.use (parser.json());
+    app.use (cookieParser());
+    app.use (expressSession ({secret: 'winteriscoming', resave: false, saveUninitialized: true, cookie: {maxAge: 60000}}));
 
-// routers config
-app.use ('/user', UserRouter);
-app.use ('/chat', ChatRouter);
 
-app.listen (3000, () => console.log ('server running on localhost:3000'));
+app.get ('/', (req, res) => {
+    res.sendFile ('index.html');
+});
+
+io.on ('connection', (socket) => {
+    socket.emit ('server-connected', {connected: true});
+    console.log ('connected');
+
+    var UserRouter = require ('./routes/user-routes');
+    var ChatRouter = require ('./routes/chat-routes')(socket);
+
+    // routers config
+    app.use ('/user', UserRouter);
+    app.use ('/chat', ChatRouter);
+});
+
+server.listen (3000, () => console.log ('server running on localhost:3000'));
--- routes/chat-routes.js
+++ routes/chat-routes.js
@@ -7,74 +7,77 @@ var config = require ('../data/config');
 
 var Chat = mongoose.model ('chats', ChatSchema);
 
-router.get ('/', (req, res) => {
-    if (req.session.user) {
-        mongoose.Promise = es6Promise;
-        mongoose.connect (config.host, config.db);
-        Chat.find ({}, (err, data) => {
-            if (err) res.send ({status: 'error', message: err});
-            else if (data && data.length > 0) {
-                res.send (data);
-            } else
-                res.send ({status: 'alert', message: 'no chats'});
-            
-            mongoose.disconnect ();
-        });
-    } else res.send ({status: 'error', message: 'Login first'});
-});
-
-router.get ('/get/:id', (req, res) => {
-    if (req.session.user) {
-        mongoose.Promise = es6Promise;
-        mongoose.connect (config.host, config.db);
-
-        Chat.findOne ({_id: req.params.id}, (err, data) => {
-            if (err) res.send ({status: 'error', message: err});
-            else if (data)
-                res.send ({status: 'success', message: data});
-            else
-                res.send ({status: 'error', message: 'no data found'});
-            
-            mongoose.disconnect ();
-        });
-    } else res.send ({status: 'error', message: 'Login first'});
-});
-
-router.post ('/message', (req, res) => {
-    if (req.session.user) {
-        if (req.body.message) {
+module.exports = (socket) => {
+    router.get ('/', (req, res) => {
+        if (req.session.user) {
             mongoose.Promise = es6Promise;
             mongoose.connect (config.host, config.db);
-            var message = new Chat({
-                _user: req.session.user.id,
-                message: req.body.message,
-                time: new Date().getTime()
+            Chat.find ({}, (err, data) => {
+                if (err) res.send ({status: 'error', message: err});
+                else if (data && data.length > 0) {
+                    res.send (data);
+                } else
+                    res.send ({status: 'alert', message: 'no chats'});
+                
+                mongoose.disconnect ();
             });
+        } else res.send ({status: 'error', message: 'Login first'});
+    });
 
-            message.save().then (() => {
-                res.send ({status: 'success', message: 'message pushed'});
-                mongoose.disconnect();
+    router.get ('/get/:id', (req, res) => {
+        if (req.session.user) {
+            mongoose.Promise = es6Promise;
+            mongoose.connect (config.host, config.db);
+
+            Chat.findOne ({_id: req.params.id}, (err, data) => {
+                if (err) res.send ({status: 'error', message: err});
+                else if (data)
+                    res.send ({status: 'success', message: data});
+                else
+                    res.send ({status: 'error', message: 'no data found'});
+                
+                mongoose.disconnect ();
             });
+        } else res.send ({status: 'error', message: 'Login first'});
+    });
+
+    router.post ('/message', (req, res) => {
+        if (req.session.user) {
+            if (req.body.message) {
+                mongoose.Promise = es6Promise;
+                mongoose.connect (config.host, config.db);
+                var message = new Chat({
+                    _user: req.session.user.id,
+                    message: req.body.message,
+                    time: new Date().getTime()
+                });
+
+                message.save().then (() => {
+                    socket.emit ('new-message', message);
+                    res.send ({status: 'success', message: 'message pushed'});
+                    mongoose.disconnect();
+                });
+            } else
+                res.send ({status:'error', message: 'cannot send empty message'});
         } else
-            res.send ({status:'error', message: 'cannot send empty message'});
-    } else
-        res.send ({status: 'error', message: 'login first'});
-});
+            res.send ({status: 'error', message: 'login first'});
+    });
 
-router.get ('/populate', (req, res) => {
-    if (req.session.user) {
-        mongoose.Promise = es6Promise;
-        mongoose.connect (config.host, config.db);
-        Chat.find({}).populate ("_user", "_id username machine").exec ((err, doc) => {
-            if (err) res.send ({status:'error', message: err});
-            else if (doc) {
-                res.send ({status: 'success', data: doc});
-            }
-            else res.send ({status: 'error', message: 'nothing found'});
+    router.get ('/populate', (req, res) => {
+        if (req.session.user) {
+            mongoose.Promise = es6Promise;
+            mongoose.connect (config.host, config.db);
+            Chat.find({}).populate ("_user", "_id username machine").exec ((err, doc) => {
+                if (err) res.send ({status:'error', message: err});
+                else if (doc) {
+                    res.send ({status: 'success', data: doc});
+                }
+                else res.send ({status: 'error', message: 'nothing found'});
 
-            mongoose.disconnect ();
-        });
-    } else res.send ({status: 'error', message: 'Login first'});
-});
+                mongoose.disconnect ();
+            });
+        } else res.send ({status: 'error', message: 'Login first'});
+    });
 
-module.exports = router;
+    return router;
+}
